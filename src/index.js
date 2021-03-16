f9ca1ca311e3ba78a1f4f94cdde5880839ff4b97
Jaimin Patel <caprizone6@gmail.com>
2021-03-16 05:32:09 -0500

    GDC graphql voyage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Voyager } from 'graphql-voyager'
+import fetch from 'isomorphic-fetch'
+import './voyager.css'
+
+function introspectionProvider() {
+
+  return fetch('./schema.json',{
+    headers : { 
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    }
+  }).then((response) => response.json())
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Voyager 
+    introspection={introspectionProvider} 
+    displayOptions={{ skipRelay: true, showLeafFields: true, hideRoot: true, rootType:  "Mutation" }}
+    workerURI={process.env.PUBLIC_URL + '/voyager.worker.js'}
+  />,
+  document.getElementById('root'),
+)
