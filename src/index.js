a128167b3fa0697ff2c5b0951bc37fffee7bed5e
Jaimin Patel <caprizone6@gmail.com>
2021-03-16 05:33:50 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ import { Voyager } from 'graphql-voyager'
 import fetch from 'isomorphic-fetch'
 import './voyager.css'
 
-function introspectionProvider() {
+function introspectionProvider(query) {
 
-  return fetch('./schema.json',{
-    headers : { 
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    }
+  return fetch('https://api.gdc.cancer.gov/v0/graphql', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: query }),
   }).then((response) => response.json())
+
+  // Alternate method to directly read json from locally stored file
+  // return fetch('./schema.json',{
+  //   headers : { 
+  //     'Content-Type': 'application/json',
+  //     'Accept': 'application/json'
+  //   }
+  // }).then((response) => response.json())
 }
 
 ReactDOM.render(
   <Voyager 
     introspection={introspectionProvider} 
-    displayOptions={{ skipRelay: true, showLeafFields: true, hideRoot: true, rootType:  "Mutation" }}
+    displayOptions={{ skipRelay: true, showLeafFields: true, rootType:  "Mutation" }}
     workerURI={process.env.PUBLIC_URL + '/voyager.worker.js'}
   />,
   document.getElementById('root'),
